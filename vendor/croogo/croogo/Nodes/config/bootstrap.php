a7dbaca581557bf6d7b469ef4217a45c863e0061
Marcin Szut <zlyjoker102@gmail.com>
2018-09-10 21:41:36 +0200

    gotowy gallery plugin

--- vendor/croogo/croogo/Nodes/config/bootstrap.php
+++ vendor/croogo/croogo/Nodes/config/bootstrap.php
@@ -24,7 +24,7 @@ Croogo::hookComponent('*', [
 ]);
 
 Croogo::hookHelper('*', 'Croogo/Nodes.Nodes');
-
+Croogo::hookHelper('*', 'Croogo/Nodes.Image2');
 // Configure Wysiwyg
 Wysiwyg::setActions([
     'Croogo/Nodes.Admin/Nodes/add' => [
